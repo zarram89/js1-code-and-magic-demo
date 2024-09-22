56b63d2692d72e6d7c26ef6dae29fc8b39edf25d
Ramil <zarram89@gmail.com>
2024-09-22 22:03:46 +0500

    Добавялет валидацию формы и pristine

--- js/user-form.js
+++ js/user-form.js
@@ -1,5 +1,24 @@
 import {getRandomArrayElement} from './util.js';
 
+const form = document.querySelector('.setup-wizard-form');
+
+const pristine = new Pristine(form, {
+  classTo: 'setup-wizard-form__element',
+  errorTextParent: 'setup-wizard-form__element',
+  errorTextClass: 'setup-wizard-form__error-text',
+});
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const isValid = pristine.validate();
+  if (isValid) {
+    console.log('Можно отправлять');
+  } else {
+    console.log('Форма невалидна');
+  }
+});
+
 const Color = {
   FIREBALLS: [
     '#ee4830',
